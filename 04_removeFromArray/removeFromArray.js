1ddb49a2c848569bb4a32aa45983a804af176bc5
fmakowa1 <fmakowa1@gmail.com>
2023-06-08 11:08:25 +0200

    completed exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, n) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++){
+        if(arr[i] !== n){
+            newArr.push(arr[i]);
+        }
+    }
+    return newArr;
 };
 
 // Do not edit below this line
